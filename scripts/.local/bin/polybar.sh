b3eb142229da6508cca04388fba4cf9c2956738d
deennyy <deennyy@protonmail.com>
2021-06-12 15:02:52 +0300

    updates

---
+++ scripts/.local/bin/polybar.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
